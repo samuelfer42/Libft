a09f0a08604ba2c6355fdef3100f5e4bd4f2a237
Samuel Fernandez <sfernand@student.42.fr>
2019-10-30 17:56:44 +0100

    Update ft_lstmap.c
    does not work [KO]

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -9,7 +9,7 @@
 /*   Updated: 2018/11/20 20:37:25 by sfernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+// 
 #include "libft.h"
 
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
